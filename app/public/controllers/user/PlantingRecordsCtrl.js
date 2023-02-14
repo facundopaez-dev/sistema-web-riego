d8ff95996d8258848d79c435fd0dfe1e5d4b6ec8
facundopaez-dev <facundo.paez.sd@gmail.com>
2023-02-13 22:14:47 -0300

    Código fuente de PlantingRecords.js correctamente identado

--- app/public/controllers/user/PlantingRecordsCtrl.js
+++ app/public/controllers/user/PlantingRecordsCtrl.js
@@ -1,56 +1,56 @@
 app.controller(
 	"PlantingRecordsCtrl",
-	["$scope","$location","$route","PlantingRecordSrv", "IrrigationRecordSrv",
-	function($scope, $location, $route, plantingRecordSrv, irrigationRecordService) {
-		console.log("Cargando PlantingRecordsCtrl...")
+	["$scope", "$location", "PlantingRecordSrv", "IrrigationRecordSrv",
+		function ($scope, $location, plantingRecordSrv, irrigationRecordService) {
+			console.log("Cargando PlantingRecordsCtrl...")
 
-		function findAll(){
-			plantingRecordSrv.findAll( function(error, data){
-				if(error){
-					alert("Ocurrió un error: " + error);
-					return;
-				}
-
-				$scope.data = data;
-			})
-		}
-
-		$scope.calculateSuggestedIrrigation = function(id){
-			plantingRecordSrv.calculateSuggestedIrrigation(id, function(error, data){
-				if(error){
-					console.log(error);
-					return;
-				}
-
-				/*
-				Si esta sentencia no esta, no se puede ver el riego
-				sugerido en el modal
-				*/
-				$scope.data = data;
-
-				/*
-				Esto impide que el modal no sea desplegado
-				*/
-				// $location.path("/plantingRecords");
-				// $route.reload();
-			});
-		}
-
-		$scope.saveIrrigationRecord = function() {
-			if ($scope.data.irrigationDone >= 0) {
-				irrigationRecordService.save($scope.data, function(error, data){
-					if(error){
-						console.log(error);
+			function findAll() {
+				plantingRecordSrv.findAll(function (error, data) {
+					if (error) {
+						alert("Ocurrió un error: " + error);
 						return;
 					}
 
 					$scope.data = data;
-				});
-			} else {
-				alert("El riego realizado sólo puede ser mayor o igual a cero");
+				})
 			}
 
-		}
+			$scope.calculateSuggestedIrrigation = function (id) {
+				plantingRecordSrv.calculateSuggestedIrrigation(id, function (error, data) {
+					if (error) {
+						console.log(error);
+						return;
+					}
 
-		findAll();
-	}]);
+					/*
+					Si esta sentencia no esta, no se puede ver el riego
+					sugerido en el modal
+					*/
+					$scope.data = data;
+
+					/*
+					Esto impide que el modal no sea desplegado
+					*/
+					// $location.path("/plantingRecords");
+					// $route.reload();
+				});
+			}
+
+			$scope.saveIrrigationRecord = function () {
+				if ($scope.data.irrigationDone >= 0) {
+					irrigationRecordService.save($scope.data, function (error, data) {
+						if (error) {
+							console.log(error);
+							return;
+						}
+
+						$scope.data = data;
+					});
+				} else {
+					alert("El riego realizado debe ser mayor o igual a cero");
+				}
+
+			}
+
+			findAll();
+		}]);
