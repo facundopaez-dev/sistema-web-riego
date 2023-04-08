3b37fc63eb33ad617c8417cd98695ce550618585
facundopaez-dev <facundo.paez.sd@gmail.com>
2023-04-07 22:33:16 -0300

    Tarea #128 terminada

--- app/src/et/Etc.java
+++ app/src/et/Etc.java
@@ -1,31 +1,26 @@
-/*
- * ETc: Evapotranspiracion del cultivo bajo condiciones estandar
- */
-
 package et;
 
 public class Etc {
 
   /**
-   * Calcula la evapotranspiracion del cultivo bajo condiciones
-   * estandar, la cual nos indica la cantidad de agua que va a evaporar
-   * un cultivo dado bajo condiciones estandar dado el coeficiente del
-   * cultivo y unos factores climaticos
-   *
-   * @param  cropCoefficient (kc)           [adimensional]
-   * @param  minTemperature                 [°C]
-   * @param  maxTemperature                 [°C]
-   * @param  pressure                       [kPa]
-   * @param  windSpeed                      [metros/segundo]
-   * @param  dewPoint (punto de rocío)      [°C]
-   * @param  extraterrestrialSolarRadiation (Ra) [MJ/metro cuadrado * dia]
-   * @param  maximumInsolation (N)          [horas]
-   * @param  cloudCover (n)                 [%]
-   * @return cantidad de agua que va a evaporar un cultivo dado [mm/dia]
+   * Retorna la evapotranspiracion del cultivo bajo condiciones
+   * estandar (ETc), la cual, es necesaria para determinar la
+   * necesidad de agua de riego de un cultivo.
+   * 
+   * La formula de la ETc es ETc = ETo (evapotranspiracion del
+   * cultivo de referencia) * kc (coeficiente de cultivo)
+   * 
+   * La formula de la evapotranspiracion del cultivo de referencia
+   * (ETo) se encuentra en la pagina 25 del libro "Evapotranspiracion
+   * del cultivo" de la FAO.
+   * 
+   * @param eto [mm/dia]
+   * @param kc  [admimensional]
+   * @return punto flotante que representa la evapotranspiracion
+   *         del cultivo bajo condiciones estandar [mm/dia]
    */
-  public static double getEtc(double cropCoefficient, double minTemperature, double maxTemperature, double pressure, double windSpeed, double dewPoint,
-    double extraterrestrialSolarRadiation, double maximumInsolation, double cloudCover) {
-    return (cropCoefficient * PenmanMonteithEto.calculateEto(minTemperature, maxTemperature, pressure, windSpeed, dewPoint, extraterrestrialSolarRadiation, maximumInsolation, cloudCover));
+  public static double calculateEtc(double eto, double kc) {
+    return eto * kc;
   }
 
 }
