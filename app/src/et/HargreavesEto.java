737b99dcd5c80e61d84f4e545742f7565eb5c88c
facundopaez-dev <facundo.paez.sd@gmail.com>
2023-04-08 18:26:39 -0300

    Tarea #129 terminada

---
+++ app/src/et/HargreavesEto.java
@@ -0,0 +1,66 @@
+package et;
+
+public class HargreavesEto {
+
+    /**
+     * Calcula la evapotranspiracion del cultivo de referencia (ETo)
+     * mediante la formula de la ETo de Hargreaves y Samani. La ETo
+     * es necesaria para calcular la necesidad de agua de riego de
+     * un cultivo.
+     * 
+     * La formula de la ETo de Hargreaves se encuentra en la pagina
+     * 64 del libro "Evapotranspiracion del cultivo" de la FAO, y es
+     * la siguiente:
+     * 
+     * ETo = 0,0023 * (Tmedia + 17,8) * (Tmax - Tmin)^0,5 + Ra
+     * 
+     * donde:
+     * - Tmedia es temperatura media
+     * - Tmax es temperatura maxima
+     * - Tmin es temperatura minima
+     * - Ra es readiacion solar extraterrestre
+     * 
+     * La necesidad hidrica de un cultivo se calcula mediante la
+     * formula de la evapotranspiracion del cultivo bajo condiciones
+     * estandar (ETc): ETc = ETo * Kc (coeficiente de cultivo).
+     * 
+     * Hay que tener en cuenta que las unidades de las variables
+     * meteorologicas dependen del servicio meteorologico utilizado.
+     * 
+     * @param maximumTemperature             [°C]
+     * @param minimumTemperature             [°C]
+     * @param extraterrestrialSolarRadiation [MJ/m2/dia]
+     * @return punto flotante que represent la evapotranspiracion
+     *         del cultivo de referencia (ETo) [mm/dia]
+     */
+    public static double calculateEto(double maximumTemperature, double minimumTemperature, double extraterrestrialSolarRadiation) {
+        return 0.0023 * (calculateMeanTemperature(maximumTemperature, minimumTemperature) + 17.8)
+                * Math.pow(maximumTemperature - minimumTemperature, 0.5)
+                * toMillimetersPerDay(extraterrestrialSolarRadiation);
+    }
+
+    /**
+     * @param maximumTemperature [°C]
+     * @param minimumTemperature [°C]
+     * @return punto flotante que representa la temperatura
+     *         media calculada a partir de una temperatura maxima
+     *         y una temperatura minima
+     */
+    private static double calculateMeanTemperature(double maximumTemperature, double minimumTemperature) {
+        return ((maximumTemperature + minimumTemperature) / 2);
+    }
+
+    /**
+     * Convierte la radiacion solar extraterrestre dada en
+     * megajulios por metro cuadrado por dia [MJ/m2/dia]
+     * a milimetros por dia.
+     * 
+     * @param extraterrestrialSolarRadiation
+     * @return punto flotante que representa la radiacion solar
+     *         extraterrestre en milimetros por dia [mm/dia]
+     */
+    private static double toMillimetersPerDay(double extraterrestrialSolarRadiation) {
+        return extraterrestrialSolarRadiation * 0.408;
+    }
+
+}
--- app/src/test/CalculateEtoTest.java
+++ app/src/test/CalculateEtoTest.java
@@ -2,11 +2,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Ignore;
 import et.ClassPenmanMonteithEtoTest;
+import et.HargreavesEto;
 
 public class CalculateEtoTest {
 
     @Test
-    public void testCalculateEto() {
+    public void testOneCalculateEto() {
         System.out.println("************************ Prueba unitaria del metodo calculateEto de la clase ClassPenmanMonteithEtoTest ************************");
         System.out.println("En esta prueba unitaria se demuestra que el metodo calculateEto de la clase ClassPenmanMonteithEtoTest calcula correctamente la");
         System.out.println("evapotranspiracion del cultivo de referencia (ETo) haciendo uso del ejemplo de la pagina 72 del libro 'Evapotranspiracion del");
@@ -34,7 +35,7 @@ public class CalculateEtoTest {
         System.out.println("- Velocidad del viento medida a 10 m de altura: " + windSpeed + " km/h");
         System.out.println("- Insolacion real (n): " + actualDurationSunstroke + " horas");
         System.out.println("- Duracion maxima de la insolacion (N): " + maximumInsolation + " horas");
-        System.out.println("- Radiacion solar extraterreste (Ra): " + extraterrestrialSolarRadiation + " MJ/metro cuadrado * dia");
+        System.out.println("- Radiacion solar extraterreste (Ra): " + extraterrestrialSolarRadiation + " MJ/m2/dia");
         System.out.println("- Presion media de vapor de la saturacion (es): " + meanSaturationVaporPressure + " [kPa]");
         System.out.println("- Presion real de vapor (ea): " + realVaporPressure + " [kPa]");
         System.out.println();
@@ -50,10 +51,47 @@ public class CalculateEtoTest {
 
         System.out.println("* Seccion de prueba *");
         System.out.println("ETo [mm/dia] esperada: " + expectedResult);
-        System.out.println("ETo [mm/dia] devuelta por el metodo calculateEto: " + eto);
+        System.out.println("ETo [mm/dia] devuelta por el metodo calculateEto de la clase ClassPenmanMonteithEtoTest: " + eto);
         System.out.println();
 
         assertEquals(expectedResult, eto, 1e-2);
+
+        System.out.println("- Prueba pasada satisfactoriamente");
+    }
+
+    @Test
+    public void testTwoCalculateEto() {
+        System.out.println("***************************** Prueba unitaria del metodo calculateEto de la clase HargreavesEto *****************************");
+        System.out.println("En esta prueba unitaria se demuestra que el metodo calculateEto de la clase HargreavesEto calcula correctamente la");
+        System.out.println("evapotranspiracion del cultivo de referencia (ETo).");
+        System.out.println();
+        System.out.println("La evapotranspiracion del cultivo de referencia (ETo) es necesaria para calcular la cantidad de agua de riego que necesita un");
+        System.out.println("cultivo. Mediante la formula de la evapotranspiracion del cultivo bajo condiciones estandar (ETc) se determina la cantidad de");
+        System.out.println("agua de riego que necesita un cultivo: ETc = ETo * Kc (coeficiente de cultivo).");
+        System.out.println();
+
+        double maximumTemperature = 29.8;
+        double minimumTemperature = 18.3;
+        double extraterrestrialSolarRadiation = 36.7;
+
+        System.out.println("Determinacion de la ETo con los siguientes datos:");
+        System.out.println("- Temperatura maxima (Tmax): " + maximumTemperature + " °C");
+        System.out.println("- Temperatura minima (Tmin): " + minimumTemperature + " °C");
+        System.out.println("- Radiacion solar extraterrestre (Ra): " + extraterrestrialSolarRadiation + " MJ/m2/dia");
+        System.out.println();
+
+        /*
+         * Seccion de prueba
+         */
+        double expectedResult = 4.91;
+        double eto = HargreavesEto.calculateEto(maximumTemperature, minimumTemperature, extraterrestrialSolarRadiation);
+
+        System.out.println("* Seccion de prueba *");
+        System.out.println("ETo [mm/dia] esperada: " + expectedResult);
+        System.out.println("ETo [mm/dia] devuelta por el metodo calculateEto de la clase HargreavesEto: " + eto);
+        System.out.println();
+
+        assertEquals(expectedResult, eto, 1e-1);
 
         System.out.println("- Prueba pasada satisfactoriamente");
     }
