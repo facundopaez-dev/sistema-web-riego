20511b9ada0f3272fda66c2dec61d0c174a70758
facundopaez-dev <facundo.paez.sd@gmail.com>
2023-04-20 05:10:30 -0300

    Tarea #154 terminada

---
+++ app/etc/sql/adminUserInserts.sql
@@ -0,0 +1,3 @@
+-- Nombre de usuario: admin, contraseña: admin
+INSERT INTO IRRIGATION_SYSTEM_USER (USERNAME, PASSWORD, NAME, LAST_NAME, EMAIL, ACTIVE, SUPERUSER)
+VALUES ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Admin name', 'Admin last name', 'admin@eservice.com', 1, 1);
