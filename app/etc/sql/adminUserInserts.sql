9838abc3722fd1ef3d91d7d47b670c871974a4de
facundopaez-dev <facundo.paez.sd@gmail.com>
2023-11-29 19:59:24 -0300

    Referencias a la inexistente tabla USER_OPTION eliminadas de archivos SQL

--- app/etc/sql/adminUserInserts.sql
+++ app/etc/sql/adminUserInserts.sql
@@ -1,5 +1,3 @@
-INSERT INTO USER_OPTION (PAST_DAYS_REFERENCE, THIRTY_DAYS_FLAG) VALUES (7, 0);
-
 -- Nombre de usuario: admin, contraseña: admin
-INSERT INTO IRRIGATION_SYSTEM_USER (USERNAME, PASSWORD, NAME, LAST_NAME, EMAIL, ACTIVE, SUPERUSER, FK_OPTION)
-VALUES ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Admin name', 'Admin last name', 'admin@eservice.com', 1, 1, 1);
+INSERT INTO IRRIGATION_SYSTEM_USER (USERNAME, PASSWORD, NAME, LAST_NAME, EMAIL, ACTIVE, SUPERUSER)
+VALUES ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Admin name', 'Admin last name', 'admin@eservice.com', 1, 1);
