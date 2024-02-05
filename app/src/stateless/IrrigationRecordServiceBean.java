1306ead1bcd3258ad794587ae3d7fcfbf3a8e19a
facundopaez-dev <facundo.paez.sd@gmail.com>
2024-02-05 04:18:39 -0300

    Paginación reimplementada en la lista de registros de riego, tarea #333

--- app/src/stateless/IrrigationRecordServiceBean.java
+++ app/src/stateless/IrrigationRecordServiceBean.java
@@ -436,8 +436,8 @@ public class IrrigationRecordServiceBean {
   }
 
   public Page<IrrigationRecord> findAllPagination(int userId, Integer page, Integer cantPerPage, Map<String, String> parameters) {
-    // Genera el WHERE dinamicante
-    StringBuffer where = new StringBuffer(" WHERE 1=1 AND u IN (SELECT r FROM IrrigationRecord r JOIN r.parcel p WHERE p IN (SELECT t FROM User u JOIN u.parcels t WHERE u.id = :userId))");
+    // Genera el WHERE dinámicamente
+    StringBuffer where = new StringBuffer(" WHERE 1=1 AND e IN (SELECT t FROM IrrigationRecord t JOIN t.parcel p WHERE p IN (SELECT x FROM User u JOIN u.parcels x WHERE u.id = :userId))");
 
     if (parameters != null) {
 
@@ -447,20 +447,20 @@ public class IrrigationRecordServiceBean {
         try {
           method = IrrigationRecord.class.getMethod("get" + capitalize(param));
 
-          if (method == null) {
+          if (method == null || parameters.get(param) == null || parameters.get(param).isEmpty()) {
             continue;
           }
 
           switch (method.getReturnType().getSimpleName()) {
             case "String":
-              where.append(" AND UPPER(");
+              where.append(" AND UPPER(e.");
               where.append(param);
-              where.append(") LIKE UPPER(");
+              where.append(") LIKE UPPER('%");
               where.append(parameters.get(param));
-              where.append(")");
+              where.append("%')");
               break;
             default:
-              where.append(" AND ");
+              where.append(" AND e.");
               where.append(param);
               where.append(" = ");
               where.append(parameters.get(param));
@@ -472,26 +472,25 @@ public class IrrigationRecordServiceBean {
           e.printStackTrace();
         }
 
-      }
+      } // End for
 
-    }
+    } // End if
 
-    // Cuenta la cantidad total de resultados
-    Query countQuery = entityManager
-        .createQuery("SELECT COUNT(u.id) FROM " + IrrigationRecord.class.getSimpleName() + " u" + where.toString());
+    // Cuenta el total de resultados
+    Query countQuery = entityManager.createQuery("SELECT COUNT(e.id) FROM " + IrrigationRecord.class.getSimpleName() + " e" + where.toString());
     countQuery.setParameter("userId", userId);
 
-    // Realiza la paginacion
-    Query query = entityManager.createQuery("FROM " + IrrigationRecord.class.getSimpleName() + " u" + where.toString());
+    // Pagina
+    Query query = entityManager.createQuery("FROM " + IrrigationRecord.class.getSimpleName() + " e" + where.toString());
     query.setMaxResults(cantPerPage);
     query.setFirstResult((page - 1) * cantPerPage);
     query.setParameter("userId", userId);
+
     Integer count = ((Long) countQuery.getSingleResult()).intValue();
     Integer lastPage = (int) Math.ceil((double) count / (double) cantPerPage);
 
     // Arma la respuesta
-    Page<IrrigationRecord> resultPage = new Page<IrrigationRecord>(page, count, page > 1 ? page - 1 : page,
-        page > lastPage ? lastPage : page + 1, lastPage, query.getResultList());
+    Page<IrrigationRecord> resultPage = new Page<IrrigationRecord>(page, count, page > 1 ? page - 1 : page, page < lastPage ? page + 1 : lastPage, lastPage, query.getResultList());
     return resultPage;
   }
 
