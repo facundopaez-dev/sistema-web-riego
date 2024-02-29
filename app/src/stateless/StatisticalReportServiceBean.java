c4ae517f33e6cca297e84ccedd897f1c2226f3a5
facundopaez-dev <facundo.paez.sd@gmail.com>
2024-02-29 02:25:16 -0300

    Reestructuración de la cadena subQuery del método findCropNamesCalculatedPerTotalNumberPlantationsPerCrop() de la clase StatisticalReportServiceBean

--- app/src/stateless/StatisticalReportServiceBean.java
+++ app/src/stateless/StatisticalReportServiceBean.java
@@ -403,8 +403,8 @@ public class StatisticalReportServiceBean {
      */
     String subQuery = "SELECT FK_CROP, COUNT(FK_CROP) AS NUMBER_PLANTATIONS FROM PLANTING_RECORD "
         + "WHERE FK_PARCEL = ?1 AND FK_STATUS = 1 AND "
-        + "((?2 <= SEED_DATE AND SEED_DATE <= ?3 AND HARVEST_DATE > ?3) "
-        + "OR (SEED_DATE >= ?2 AND HARVEST_DATE <= ?3) OR "
+        + "((?2 <= SEED_DATE AND SEED_DATE <= ?3 AND HARVEST_DATE > ?3) OR "
+        + "(SEED_DATE >= ?2 AND HARVEST_DATE <= ?3) OR "
         + "(?2 <= HARVEST_DATE AND HARVEST_DATE <= ?3 AND SEED_DATE < ?2)) "
         + "GROUP BY FK_CROP";
 
