dd0b7d3ffcfd79c6bc553c78e2b055fe721a3780
facundopaez-dev <facundo.paez.sd@gmail.com>
2024-03-16 03:04:24 -0300

    Tarea #395 terminada

--- app/src/stateless/ClimateRecordServiceBean.java
+++ app/src/stateless/ClimateRecordServiceBean.java
@@ -839,7 +839,7 @@ public class ClimateRecordServiceBean {
     countQuery.setParameter("userId", userId);
 
     // Pagina
-    Query query = entityManager.createQuery("FROM " + ClimateRecord.class.getSimpleName() + " e" + where.toString());
+    Query query = entityManager.createQuery("FROM " + ClimateRecord.class.getSimpleName() + " e" + where.toString() + " ORDER BY e.date");
     query.setMaxResults(cantPerPage);
     query.setFirstResult((page - 1) * cantPerPage);
     query.setParameter("userId", userId);
