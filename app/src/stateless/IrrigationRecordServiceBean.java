4c356b63d1bdd256dfbc301f4c3c5e2cffbe2eda
facundopaez-dev <facundo.paez.sd@gmail.com>
2024-03-26 04:14:37 -0300

    Tarea #416 terminada

--- app/src/stateless/IrrigationRecordServiceBean.java
+++ app/src/stateless/IrrigationRecordServiceBean.java
@@ -776,7 +776,7 @@ public class IrrigationRecordServiceBean {
     countQuery.setParameter("userId", userId);
 
     // Pagina
-    Query query = entityManager.createQuery("FROM " + IrrigationRecord.class.getSimpleName() + " e" + where.toString());
+    Query query = entityManager.createQuery("FROM " + IrrigationRecord.class.getSimpleName() + " e" + where.toString() + " ORDER BY e.date");
     query.setMaxResults(cantPerPage);
     query.setFirstResult((page - 1) * cantPerPage);
     query.setParameter("userId", userId);
