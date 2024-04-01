0ab57215bde23195315581b67fd9b28b9782e8bd
facundopaez-dev <facundo.paez.sd@gmail.com>
2024-03-31 21:46:56 -0300

    Clase de pruebas unitarias ClimateClientTest arreglada y en funcionamiento

--- app/src/test/ClimateClientTest.java
+++ app/src/test/ClimateClientTest.java
@@ -1,17 +1,21 @@
 import static org.junit.Assert.*;
 
-import java.io.IOException;
 import java.util.Calendar;
-import org.junit.AfterClass;
+import java.util.Collection;
+import java.util.ArrayList;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.Ignore;
 import model.ClimateRecord;
+import model.GeographicLocation;
 import model.Parcel;
+import model.TypePrecipitation;
 import util.UtilDate;
 import climate.ClimateClient;
 
 public class ClimateClientTest {
+
+  private static Collection<TypePrecipitation> typesPrecip;
 
   private static final int JANUARY = 0;
   private static final int FEBRUARY = 1;
@@ -26,26 +30,54 @@ public class ClimateClientTest {
   private static final int NOVEMBER = 10;
   private static final int DECEMBER = 11;
 
+  @BeforeClass
+  public static void preTest(){
+    TypePrecipitation typePrecipOne = new TypePrecipitation();
+    typePrecipOne.setName("rain");
+    typePrecipOne.setSpanishName("Lluvia");
+
+    TypePrecipitation typePrecipTwo = new TypePrecipitation();
+    typePrecipTwo.setName("ice");
+    typePrecipTwo.setSpanishName("Granizo");
+
+    TypePrecipitation typePrecipThree = new TypePrecipitation();
+    typePrecipThree.setName("snow");
+    typePrecipThree.setSpanishName("Nieve");
+
+    TypePrecipitation typePrecipFour = new TypePrecipitation();
+    typePrecipFour.setName("freezing rain");
+    typePrecipFour.setSpanishName("Lluvia gélida");
+
+    typesPrecip = new ArrayList<>();
+    typesPrecip.add(typePrecipOne);
+    typesPrecip.add(typePrecipTwo);
+    typesPrecip.add(typePrecipThree);
+    typesPrecip.add(typePrecipFour);
+  }
+
   @Test
-  public void testOneClimateClient() {
+  public void testOne() {
     Calendar date = Calendar.getInstance();
     date.set(Calendar.YEAR, 2023);
     date.set(Calendar.MONTH, DECEMBER);
     date.set(Calendar.DAY_OF_MONTH, 31);
 
-    double latitude = -45.86413;
-    double longitude = -67.49656;
+    double latitude = 38.89511;
+    double longitude = -77.03637;
 
     System.out.println("***************************** Prueba uno del cliente climatico *****************************");
     System.out.println("Pronostico");
     System.out.println("- En esta prueba se obtienen los datos meteorologicos para la ubicacion geografica dada por");
-    System.out.println("la coordenada geografica (" + latitude + ", " + longitude + ") [Comodoro Rivadavia] en la fecha " + UtilDate.convertDateToDdMmYyyy(date) + ".");
+    System.out.println("la coordenada geografica (" + latitude + ", " + longitude + ") [Washington] en la fecha " + UtilDate.convertDateToDdMmYyyy(date) + ".");
     System.out.println();
 
+    GeographicLocation newGeographicLocation = new GeographicLocation();
+    newGeographicLocation.setLatitude(latitude);
+    newGeographicLocation.setLongitude(longitude);
+
     Parcel newParcel = new Parcel();
-    newParcel.setName("Comodoro Rivadavia");
-    newParcel.setLatitude(latitude);
-    newParcel.setLongitude(longitude);
+    newParcel.setName("Washington");
+    newParcel.setGeographicLocation(newGeographicLocation);
     newParcel.setHectares(2);
 
     /*
@@ -54,7 +86,7 @@ public class ClimateClientTest {
     ClimateRecord climateRecord = null;
 
     try {
-      climateRecord = ClimateClient.getForecast(newParcel, date);
+      climateRecord = ClimateClient.getForecast(newParcel, date, typesPrecip);
     } catch (Exception e) {
       System.out.println("Codigo de estado de respuesta HTTP: " + e.getMessage());
     }
@@ -69,25 +101,28 @@ public class ClimateClientTest {
   }
 
   @Test
-  public void testTwoClimateClient() {
+  public void testTwo() {
     Calendar date = Calendar.getInstance();
     date.set(Calendar.YEAR, 2023);
     date.set(Calendar.MONTH, JANUARY);
     date.set(Calendar.DAY_OF_MONTH, 1);
 
-    double latitude = -38.71959;
-    double longitude = -62.27243;
+    double latitude = 40.4165;
+    double longitude = -3.70256;
 
     System.out.println("***************************** Prueba dos del cliente climatico *****************************");
     System.out.println("Observacion historica");
     System.out.println("- En esta prueba se obtienen los datos meteorologicos para la ubicacion geografica dada por");
-    System.out.println("la coordenada geografica (" + latitude + ", " + longitude + ") [Bahia Blanca] en la fecha " + UtilDate.convertDateToDdMmYyyy(date) + ".");
+    System.out.println("la coordenada geografica (" + latitude + ", " + longitude + ") [Madrid] en la fecha " + UtilDate.convertDateToDdMmYyyy(date) + ".");
     System.out.println();
 
+    GeographicLocation newGeographicLocation = new GeographicLocation();
+    newGeographicLocation.setLatitude(latitude);
+    newGeographicLocation.setLongitude(longitude);
+
     Parcel newParcel = new Parcel();
-    newParcel.setName("Bahia Blanca");
-    newParcel.setLatitude(latitude);
-    newParcel.setLongitude(longitude);
+    newParcel.setName("Madrid");
+    newParcel.setGeographicLocation(newGeographicLocation);
     newParcel.setHectares(2);
 
     /*
@@ -96,7 +131,7 @@ public class ClimateClientTest {
     ClimateRecord climateRecord = null;
 
     try {
-      climateRecord = ClimateClient.getForecast(newParcel, date);
+      climateRecord = ClimateClient.getForecast(newParcel, date, typesPrecip);
     } catch (Exception e) {
       System.out.println("Codigo de estado de respuesta HTTP: " + e.getMessage());
     }
@@ -111,21 +146,24 @@ public class ClimateClientTest {
   }
 
   @Test
-  public void testThreeClimateClient() {
-    double latitude = -42.7692;
-    double longitude = -65.03851;
+  public void testThree() {
+    double latitude = 51.50853;
+    double longitude = -0.12574;
 
     System.out.println("***************************** Prueba tres del cliente climatico *****************************");
     System.out.println("Pronostico de condiciones actuales");
     System.out.println("- En esta prueba se obtienen los datos meteorologicos para la ubicacion geografica dada por");
-    System.out.println("la coordenada geografica (" + latitude + ", " + longitude + ") [Puerto Madryn] en la fecha actual (es decir,");
+    System.out.println("la coordenada geografica (" + latitude + ", " + longitude + ") [Londres] en la fecha actual (es decir,");
     System.out.println("hoy).");
     System.out.println();
 
+    GeographicLocation newGeographicLocation = new GeographicLocation();
+    newGeographicLocation.setLatitude(latitude);
+    newGeographicLocation.setLongitude(longitude);
+
     Parcel newParcel = new Parcel();
-    newParcel.setName("Puerto Madryn");
-    newParcel.setLatitude(latitude);
-    newParcel.setLongitude(longitude);
+    newParcel.setName("Londres");
+    newParcel.setGeographicLocation(newGeographicLocation);
     newParcel.setHectares(2);
 
     Calendar currentDate = UtilDate.getCurrentDate();
@@ -136,7 +174,7 @@ public class ClimateClientTest {
     ClimateRecord climateRecord = null;
 
     try {
-      climateRecord = ClimateClient.getForecast(newParcel, currentDate);
+      climateRecord = ClimateClient.getForecast(newParcel, currentDate, typesPrecip);
     } catch (Exception e) {
       System.out.println("Codigo de estado de respuesta HTTP: " + e.getMessage());
     }
