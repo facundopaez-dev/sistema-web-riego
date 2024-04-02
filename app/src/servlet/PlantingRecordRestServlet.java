6e0d6f2b0e150ee8a260e820134de80aaf2de986
facundopaez-dev <facundo.paez.sd@gmail.com>
2024-04-02 16:48:22 -0300

    Utilización del método calculateTotalAmountWaterAvailable() (cálculo de la capacidad de almacenamiento de agua del suelo) de la clase WaterMath para el aviso de la muerte de un cultivo

--- app/src/servlet/PlantingRecordRestServlet.java
+++ app/src/servlet/PlantingRecordRestServlet.java
@@ -2540,15 +2540,16 @@ public class PlantingRecordRestServlet {
      */
     if (stringIrrigationWaterNeedCurrentDate != null && stringIrrigationWaterNeedCurrentDate.equals(notCalculated)) {
       plantingRecordService.updateCropIrrigationWaterNeed(developingPlantingRecord.getId(), notAvailable);
+
       Calendar cropDeathDate = plantingRecordService.find(developingPlantingRecord.getId()).getDeathDate();
+      double totalAmountWaterAvailable = WaterMath.calculateTotalAmountWaterAvailable(
+          developingPlantingRecord.getCrop(), developingPlantingRecord.getParcel().getSoil());
 
       String message = "El cultivo murió porque la pérdida de humedad del suelo fue estrictamente mayor al doble"
-          + " de la capacidad de almacenamiento de agua del suelo (2 * "
-          + developingPlantingRecord.getTotalAmountWaterAvailable() + " mm"
-          + " = " + (2 * developingPlantingRecord.getTotalAmountWaterAvailable()) + " mm). A partir de la fecha "
-          + UtilDate.formatDate(cropDeathDate)
-          + " la pérdida de humedad del suelo"
-          + " comenzó a ser de " + soilWaterBalanceService.calculateCropDeathSoilMoistureLoss(developingPlantingRecord.getParcel().getId(), cropDeathDate) 
+          + " de la capacidad de almacenamiento de agua del suelo (2 * " + totalAmountWaterAvailable + " mm"
+          + " = " + (2 * totalAmountWaterAvailable) + " mm). A partir de la fecha " + UtilDate.formatDate(cropDeathDate)
+          + " la pérdida de humedad del suelo comenzó a ser de "
+          + soilWaterBalanceService.calculateCropDeathSoilMoistureLoss(developingPlantingRecord.getParcel().getId(), cropDeathDate) 
           + " mm. El gráfico de la evolución diaria del nivel de humedad del suelo puede ser visualizado para un"
           + " cultivo muerto visualizando el respectivo registro de plantación.";
 
